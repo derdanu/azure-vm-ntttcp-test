8acf9d2836a838d72372e5482d5f01df21be01e7
Daniel Falkner <dafalkne@microsoft.com>
2022-02-23 13:22:06 +0100

    single qoutes in the output.

--- outputs.tf
+++ outputs.tf
@@ -10,12 +10,12 @@ output "instance2_ip_addr" {
 
 
 output "receiver_cmd" {
-  value       = "sshpass -p ${var.admin_password} ssh -o \"StrictHostKeyChecking no\" -l ${var.admin_username} ${azurerm_public_ip.publicip1.ip_address} /usr/local/bin/ntttcp -r -t 300"
+  value       = "sshpass -p ${var.admin_password} ssh -o 'StrictHostKeyChecking no' -l ${var.admin_username} ${azurerm_public_ip.publicip1.ip_address} /usr/local/bin/ntttcp -r -t 300"
   description = "Commandline to start the Receiver"
 }
 
 
 output "sender_cmd" {
-  value       = "sshpass -p ${var.admin_password} ssh -o \"StrictHostKeyChecking no\" -l ${var.admin_username} ${azurerm_public_ip.publicip2.ip_address} /usr/local/bin/ntttcp -s 10.0.1.4 -t 300"
+  value       = "sshpass -p ${var.admin_password} ssh -o 'StrictHostKeyChecking no' -l ${var.admin_username} ${azurerm_public_ip.publicip2.ip_address} /usr/local/bin/ntttcp -s 10.0.1.4 -t 300"
   description = "Commandline to start the Sender"
 }
